ab7c67d38fd1a9e63a710556b0f7a1faaf4efd1e
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-16 01:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let num=parseInt(prompt("enter num : "))
+let result=floor(n/2);
+let max=-Infinite;
+let map=new Map();
+for(let t of num){
+	if(map.has(t)){
+		let v=map.get(t,1);
+		map.set(t,v+1);
+	}
+	else{
+		map.set(t,1);
+	}
+}
+for(let [key,value] of map){
+	if(max<value)
+	{
+		max=value;
+	}
+}
+if(max>result){
+	return max;
+}
